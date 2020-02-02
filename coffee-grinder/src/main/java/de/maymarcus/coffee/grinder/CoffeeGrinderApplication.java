34bd5f0cfa4eccdbd862e4428fdd978e74d2dd56
mayms <mail@maymarcus.de>
2020-02-02 22:14:04 +0100

    adds java project with spring boot

---
+++ coffee-grinder/src/main/java/de/maymarcus/coffee/grinder/CoffeeGrinderApplication.java
@@ -0,0 +1,13 @@
+package de.maymarcus.coffee.grinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeGrinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeGrinderApplication.class, args);
+	}
+
+}
---
+++ coffee-grinder/src/test/java/de/maymarcus/coffee/grinder/CoffeeGrinderApplicationTests.java
@@ -0,0 +1,13 @@
+package de.maymarcus.coffee.grinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeGrinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
